f7c38ad90561bf9c3e0cd62f9361c62a01946597
Mashinsouru <aythaSG@gmail.com>
2023-08-31 12:51:18 +0100

    creacion de constructor de player

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Pam')
+var mainBoard = document.getElementById('main-board')
+function Player(){
+    this.x =10
+    this.y = 295
+    this.sprite = document.createElement('div')
+
+    this.insertPlayer = function (){
+        this.sprite.setAttribute('id', 'player')
+        this.sprite.style.top = this.y + 'px'
+        this.sprite.style.left = this.x + 'px'
+    mainBoard.appendChild(this.sprite)
+    }    
+}
