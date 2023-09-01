012ecda24255c5b86944fe57e25f77371922cb6f
Alvaro Estevez <alvaro5estevez@gmail.com>
2023-09-01 10:55:59 +0100

    Create fruits, see, move and collisions

---
+++ fruit.js
@@ -0,0 +1,46 @@
+var mainBoard = document.getElementById('main-board')
+function Fruit(player) {
+    var self = this
+    this.x = 1320
+    this.y = Math.ceil(Math.random() * 630)
+    this.width = 25
+    this.height = 25
+    this.direction = 0
+    this.sprite = document.createElement('div')
+
+    this.insertFruit = function () {
+        this.sprite.setAttribute('id', 'fruit')
+        this.sprite.style.top = this.y + 'px'
+        this.sprite.style.left = this.x + 'px'
+        mainBoard.appendChild(this.sprite)
+    }
+    this.move = function () {
+        self.checkcollision()
+        if (self.x > -150) {
+            self.x -= 5
+            self.sprite.style.left = self.x + "px"
+        } else {
+            self.removeFruit()
+        }
+
+
+    }
+    this.removeFruit = function () {
+        mainBoard.removeChild(this.sprite)
+        clearInterval(this.timerId)
+
+    }
+    this.checkcollision = function () {
+        if (
+            this.x + this.width >= player.x &&
+            this.x <= player.x + player.width
+            &&
+            this.y + this.height >= player.y &&
+            this.y <= player.y + player.height
+        ) {
+            this.removeFruit()
+        }
+    }
+    this.timerId = setInterval(this.move, 20)
+}
+export { Fruit }
--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 import { Player } from "./player.js"
 import { Enemy } from "./enemy.js"
+import { Fruit } from "./fruit.js"
 
 
 var player1 = new Player()
 player1.insertPlayer()
 function newEnemy(){
- var enemy1 = new Enemy(player1)
-enemy1.insertEnemy()
+    var enemy1 = new Enemy(player1)
+    enemy1.insertEnemy()
+}
+function newFruit() {
+    var fruit1 = new Fruit(player1)
+    fruit1.insertFruit()
 }
 
 
 window.addEventListener('keydown', function (e) {
+    console.log(e.key)
     switch (e.key) {
         case "a":
             player1.direction = "left"
@@ -27,12 +33,14 @@ window.addEventListener('keydown', function (e) {
     }
 })
 window.addEventListener('keyup', function (e) {
+    console.log(e.key)
     player1.direction = 0
 })
 
 function start (){
-    var timerIdPlayer = setInterval(player1.move, 10, player1.direction)
+    var timerIdPlayer = setInterval(player1.move, 10)
     var timerIdEnemy = setInterval(newEnemy, 500)
+    var timerIdEnemy = setInterval(newFruit, 10000)
 }
 start()
 
